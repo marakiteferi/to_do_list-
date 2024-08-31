c166e35131ce3a796a00fbfdcb33c16bed516d5a
eden-github888 <edenwekele@gmail.com>
2024-08-31 14:32:48 +0300

    added model file

---
+++ app/Models/Task.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'priority',
+        'category_id',
+        'status_id',
+    ];
+
+    protected $casts = [
+        'priority' => 'string',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+}
