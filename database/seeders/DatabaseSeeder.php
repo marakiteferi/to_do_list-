7d45bc4fce42b274b2781ea4b17de15f3b968fe3
eden-github888 <edenwekele@gmail.com>
2024-09-04 14:23:26 +0300

    seeders added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,24 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Call individual seeders
+        $this->call([
+            UsersTableSeeder::class,
+            StatusesTableSeeder::class,
+            CategoriesTableSeeder::class,
+            TasksTableSeeder::class,
         ]);
     }
 }
